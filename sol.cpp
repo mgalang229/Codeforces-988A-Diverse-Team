5b8b3bd6a4cc22a1f479aaae5feb3d8ff4023071
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-07-16 15:10:45 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-typedef long long ll;
-
 void test_case() {
 	int n, k;
 	scanf("%d%d", &n, &k);
@@ -13,7 +11,7 @@ void test_case() {
 		int x;
 		scanf("%d", &x);
 		if(!el.count(x)) {
-			ans.push_back(i);
+			ans.push_back(i + 1);
 			el.insert(x);
 		}
 	}
@@ -23,18 +21,13 @@ void test_case() {
 	else {
 		printf("YES\n");
 		for(int i = 0; i < k; ++i) {
-			printf("%d ", ans[i] + 1);
+			printf("%d ", ans[i]);
 		}
 		printf("\n");
 	}
 }
 
 int main() {
-	//int T;
-	//scand("%d", &T);
-	//for(int nr = 1; nr <= T; nr++) {
-		//test_case();
-	//}
 	test_case();
 	return 0;
 }
